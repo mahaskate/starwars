041e9c406dfb05a6effe3cc3f51921780dde94c7
Daniel Cevallos <mahaskate@gmail.com>
2012-12-05 00:55:53 -0200

    jj

--- ship/core.php
+++ ship/core.php
@@ -62,9 +62,10 @@ function param($num){
 
 //conecta no banco de dados
 header('Content-Type: text/html; charset=utf-8');
-$link = mysql_connect ($servidor,$usuario_bd) or die ("Erro ao conectar no banco de dados: ".mysql_error());
+/*$link = mysql_connect ($servidor,$usuario_bd) or die ("Erro ao conectar no banco de dados: ".mysql_error());
 mysql_set_charset("utf8",$link);
 $db = mysql_select_db ($bd) or die ("Erro ao encontrar o banco de dados: ".mysql_error());
+*/
 
 function element($element) {
 	global $root;
