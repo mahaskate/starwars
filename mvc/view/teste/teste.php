9881b9782103bc7964689f23c64f0451feebaff3
Daniel Cevallos <mahaskate@gmail.com>
2012-12-06 00:40:11 -0200

    mm

--- mvc/view/teste/teste.php
+++ mvc/view/teste/teste.php
@@ -3,8 +3,12 @@ $var = param(0);
 
 echo "view teste";
 echo "<br>";
-echo "parametro - ".$var;
-
-echo root();
+echo "parametro --- ".$var;
+echo "<br>";
+$oi = $_SERVER['PHP_SELF'];
+echo $oi;
+$oi = explode("/",$oi);
+echo "<br>";
+echo $oi[1]
 
 ?>
