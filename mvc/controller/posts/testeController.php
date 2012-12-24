4d415908d66442c0700455d7cab4f0cae00cc4d2
Daniel Cevallos <mahaskate@gmail.com>
2012-12-24 00:46:08 -0200

    mm

--- mvc/controller/posts/testeController.php
+++ mvc/controller/posts/testeController.php
@@ -5,6 +5,21 @@
 
 //echo dirname(__FILE__);
 
+	require_once '../mvc/controller/components/wideImage/lib/Exception.php';
+	
+	require_once '../mvc/controller/components/wideImage/lib/Image.php';
+	require_once '../mvc/controller/components/wideImage/lib/TrueColorImage.php';
+	require_once '../mvc/controller/components/wideImage/lib/PaletteImage.php';
+	
+	require_once '../mvc/controller/components/wideImage/lib/Coordinate.php';
+	require_once '../mvc/controller/components/wideImage/lib/Canvas.php';
+	require_once '../mvc/controller/components/wideImage/lib/MapperFactory.php';
+	require_once '../mvc/controller/components/wideImage/lib/OperationFactory.php';
+	
+	require_once '../mvc/controller/components/wideImage/lib/Font/TTF.php';
+	require_once '../mvc/controller/components/wideImage/lib/Font/GDF.php';
+	require_once '../mvc/controller/components/wideImage/lib/Font/PS.php';
+
 include "../mvc/controller/components/wideImage/lib/wideImage.php";
 
 if($_POST){
