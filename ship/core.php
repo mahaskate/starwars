1b0226f8a58de998d8c8ec0b4c012920bc91528b
Daniel Cevallos <mahaskate@gmail.com>
2012-12-24 14:28:22 -0200

    mm

--- ship/core.php
+++ ship/core.php
@@ -24,7 +24,7 @@ require "../vars/vars.php";
 
 //conecta no banco de dados
 header('Content-Type: text/html; charset=utf-8');
-$link = mysql_connect ($servidor,$usuario_bd) or die ("Erro ao conectar no banco de dados: ".mysql_error());
+$link = mysql_connect ($servidor,$usuario_bd,$senha_bd) or die ("Erro ao conectar no banco de dados: ".mysql_error());
 mysql_set_charset("utf8",$link);
 $db = mysql_select_db ($bd) or die ("Erro ao encontrar o banco de dados: ".mysql_error());
 
