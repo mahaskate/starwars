0e98c976818a7de7ac671265900bf3dd871075e8
Daniel Cevallos <mahaskate@gmail.com>
2012-12-23 23:46:17 -0200

    mm

--- mvc/controller/posts/testeController.php
+++ mvc/controller/posts/testeController.php
@@ -1,7 +1,7 @@
 <?php
 
 if($_POST){
-	require component('wideImage/lib/wideImage.php');
+	require '../mvc/controller/components/wideImage/lib/wideImage.php');
 
 	wideImage::loadFromUpload('foto')->resize(50,50)->saveToFile(path('img/'.$_FILES['foto']['name']));
 }
