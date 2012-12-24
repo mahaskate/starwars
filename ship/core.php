89d2d35dbe8ca52b94728259938463a3df3e8571
Daniel Cevallos <mahaskate@gmail.com>
2012-12-24 14:43:42 -0200

    mm

--- ship/core.php
+++ ship/core.php
@@ -20,25 +20,7 @@ require "uploadFiles.php";
 require "components.php";
 //Carrega variaveis de configuração
 require "../vars/vars.php";
-//require "database.php";
-
-
-
-
-//conecta no banco de dados
-header('Content-Type: text/html; charset=utf-8');
-
-
-
-
-$link = mysql_connect ('tunnel.pagodabox.com','cori','oWrju9HG') or die ("Erro ao conectar no banco de dados: ".mysql_error());
-mysql_set_charset("utf8",$link);
-$db = mysql_select_db ('dani') or die ("Erro ao encontrar o banco de dados: ".mysql_error());
-
-
-
-
-
+require "database.php";
 //Carrega rota
 require "../router/rotas.php";
 
