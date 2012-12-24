f7a417ba3521043de00f793a588d71776173d986
Daniel Cevallos <mahaskate@gmail.com>
2012-12-24 14:36:00 -0200

    mm

--- ship/core.php
+++ ship/core.php
@@ -31,7 +31,7 @@ header('Content-Type: text/html; charset=utf-8');
 
 
 
-$link = mysql_connect ('127.0.0.1','cori','oWrju9HG') or die ("Erro ao conectar no banco de dados: ".mysql_error());
+$link = mysql_connect ('tunnel.pagodabox.com','cori','oWrju9HG') or die ("Erro ao conectar no banco de dados: ".mysql_error());
 mysql_set_charset("utf8",$link);
 $db = mysql_select_db ('dani') or die ("Erro ao encontrar o banco de dados: ".mysql_error());
 
