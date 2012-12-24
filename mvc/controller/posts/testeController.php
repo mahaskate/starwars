4ac95e0058207195b826e8452e63bab4cbedd54b
Daniel Cevallos <mahaskate@gmail.com>
2012-12-24 00:19:40 -0200

    mm

--- mvc/controller/posts/testeController.php
+++ mvc/controller/posts/testeController.php
@@ -3,7 +3,7 @@
 //echo $_SERVER['SCRIPT_NAME'];
 //require "../mvc/controller/components/wideImage/lib/wideImage.php";
 
-include "../mvc/controller/components/wideImage/lib/teste.php";
+include "../mvc/controller/components/wideImage/lib/wideImage.php";
 
 if($_POST){
 
