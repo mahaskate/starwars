285698004747aa2c88b353552064bfc77554e530
Daniel Cevallos <mahaskate@gmail.com>
2012-12-24 02:49:25 -0200

    mm

--- mvc/controller/posts/testeController.php
+++ mvc/controller/posts/testeController.php
@@ -1,6 +1,12 @@
 <?php
 
-require component('facebook/core.php');
+require "../mvc/controller/components/facebook/src/base_facebook.php";
+require "../mvc/controller/components/facebook/src/facebook.php";
+
+$facebook = new Facebook(array(
+  'appId'  => '404699592875095',
+  'secret' => '7a77bc1476389c95a89b0094a1fd380f',
+));
 
 $me = $facebook->api('/cevalinho');
 
